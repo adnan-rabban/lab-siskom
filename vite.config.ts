f1aad0eb248051ee1a5bc5bf193e4c1864bb8fe1
adun <adnan2765rabbani@gmail.com>
2026-05-25 00:52:33 +0700

    [2026-05-25] chore(deploy): trigger github actions build workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Trigger GitHub Actions build
 export default defineConfig({
   base: '/lab-siskom/',
   plugins: [react()],
